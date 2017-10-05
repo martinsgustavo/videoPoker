83b4def2aa82c0587e1267956583c85a9ca20646
Gustavo Martins <martinsgustavo@MacBook-Pro-de-Gustavo.local>
2017-10-05 08:39:38 -0400

    Initial Commit

---
+++ videoPoker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  videoPoker
+//
+//  Created by Gustavo Martins on 17-10-05.
+//  Copyright © 2017 Gustavo Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
